31c3f98509b95ca88249596031d25257c5519077
mateoalej96 <mateoalej96@gmail.com>
2025-02-04 22:39:37 +0000

    first commit

---
+++ src/app/Views/product-list/product-list.component.ts
@@ -0,0 +1,29 @@
+import { Component, ElementRef } from '@angular/core';
+
+@Component({
+  selector: 'app-product-list',
+  imports: [],
+  templateUrl: './product-list.component.html',
+  styleUrl: './product-list.component.css'
+})
+export class ProductListComponent {
+  ngAfterViewInit() {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add('visible');
+          } else {
+            entry.target.classList.remove('visible');
+          }
+        });
+      },
+      {
+        threshold: 0.1, // Un 10% visible para activar la animación
+      }
+    );
+
+    const productElements = document.querySelectorAll('.product');
+    productElements.forEach((el) => observer.observe(el));
+  }
+}
